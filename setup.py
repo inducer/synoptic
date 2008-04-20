2a3c57bb3717d030fc71ef70067258d7c2b356b7
Andreas Kloeckner <inform@tiker.net>
2008-04-20 11:09:09 -0400

    Fix long_description in setup.py

--- setup.py
+++ setup.py
@@ -18,29 +18,29 @@ def main():
           The following features set it apart:
 
           * **Fully versioned.** Never deletes *anything*, *ever*. If you want to go back
-          to a previous version of something, just drag that slider up there in the
-          top-left corner.
+            to a previous version of something, just drag that slider up there in the
+            top-left corner.
 
           * **Super-simple Navigation.** Adaptive tag clouds, 
-          support for the forward/back button on your browser, 
-          query links, support for browser bookmarks. All to make sure
-          you can find that note when you need it.
+            support for the forward/back button on your browser, 
+            query links, support for browser bookmarks. All to make sure
+            you can find that note when you need it.
 
           * **Powerful searching.** Synoptic is meant to keep *large*
-          note collections manageable and accessible. You can search for items
-          based on tags, their creation time, or even search through their *full
-          text*. Plus arbitrary logical combinations of them, using the 
-          logical operatos `and`, `or`, and `not`.
+            note collections manageable and accessible. You can search for items
+            based on tags, their creation time, or even search through their *full text*. 
+            Plus arbitrary logical combinations of them, using the 
+            logical operatos and, or, and not.
 
           * **Easy Markup.** Synoptic uses
-          Markdown to allow you to type formatted notes easily and
-          quickly. Plus, there are a few extensions to facilitate
-          typing math.
+            Markdown to allow you to type formatted notes easily and
+            quickly. Plus, there are a few extensions to facilitate
+            typing math.
 
           * **Advanced Features.** A lot of refinement work has gone into
-          making Synoptic work as seamlessly as possible. You may never notice
-          many of these refinements, because they're meant to make stuff work like
-          it's supposed to. If you're curious nonetheless, take a peek.
+            making Synoptic work as seamlessly as possible. You may never notice
+            many of these refinements, because they're meant to make stuff work like
+            it's supposed to.
           """,
           author=u"Andreas Kloeckner",
           author_email="inform@tiker.net",
