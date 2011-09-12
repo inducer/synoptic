b7c6a64dfd7d312c34ce6cb76e17c2fcb3db2609
Andreas Kloeckner <inform@tiker.net>
2011-09-11 22:24:10 -0400

    Mockup work on synoptic mobile.

--- synoptic/html.py
+++ synoptic/html.py
@@ -94,6 +94,7 @@ def mobile_page(context):
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head>
       <title>%(title)s</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="icon" type="image/png" href="static/synoptic.png"/>
       <link rel="stylesheet" type="text/css" href="static/jquery-mobile/jquery.mobile-1.0b3.css" media="screen"/>
       <script type="text/javascript" src="app/get_all_js"></script>
@@ -211,49 +212,57 @@ def main_page(context):
 
 def mobile_body():
     return """
-    <!-- Start of first page: #one -->
-    <div data-role="page" id="foo">
+    <div data-role="page" id="main">
 
         <div data-role="header">
-            <h1>Multi-page</h1>
-        </div><!-- /header -->
+            <h1>Synoptic Mobile</h1>
+        </div>
 
         <div data-role="content" id="one">
-            <h2>One</h2>
+            <div data-role="fieldcontain">
+                <input type="search" name="query" id="search" value="" />
+            </div>
 
-            <p>I have an id of "one" on my page container. I'm first in the source order so I'm shown when the page loads.</p>
+            <ul data-role="listview">
+                <li><a href="index.html">
+                    <h3>Stephen Weber</h3>
+                    <p><strong>You've been invited to a meeting at Filament Group in Boston, MA</strong></p>
+                    <p>Hey Stephen, if you're available at 10am tomorrow, we've got a meeting with the jQuery team.</p>
+                    <p class="ui-li-aside"><strong>6:24</strong>PM</p>
 
-            <p>This is a multi-page boilerplate template that you can copy to build you first jQuery Mobile page. This template contains multiple "page" containers inside, unlike a <a href="page-template.html"> single page template</a> that has just one page within it.</p>
-            <p>Just view the source and copy the code to get started. All the CSS and JS is linked to the jQuery CDN versions so this is super easy to set up. Remember to include a meta viewport tag in the head to set the zoom level.</p>
+                </a></li>
+                <li><a href="index.html">
+                    <h3>jQuery Team</h3>
+                    <p><strong>Boston Conference Planning</strong></p>
+                    <p>In preparation for the upcoming conference in Boston, we need to start gathering a list of sponsors and speakers.</p>
+                    <p class="ui-li-aside"><strong>9:18</strong>AM</p>
 
-            <p>You link to internal pages by referring to the ID of the page you want to show. For example, to <a href="#two" >link</a> to the page with an ID of "two", my link would have a <code>href="#two"</code> in the code.</p>
+                </a></li>
+            </ul>
+            <p style="margin-top:15px"></p>
 
-            <h3>Show internal pages:</h3>
-            <p><a href="#two" data-role="button">Show page "two"</a></p>
-            <p><a href="#popup"data-role="button" data-rel="dialog" data-transition="pop">Show page "popup" (as a dialog)</a></p>
+            <div data-role="controlgroup">
+            <a href="#calendar" data-role="button" data-icon="grid">View Calendar</a>
+            <a href="#tags" data-role="button" data-icon="info">Browse Tags</a>
+            <a href="#tags" data-role="button" data-icon="arrow-l">Go Back in Time</a>
+            <a href="#quit" data-role="button" data-icon="alert">Quit Synoptic</a>
+            </div>
         </div><!-- /content -->
 
-
-        <div data-role="footer" data-theme="d">
-            <h4>Page Footer</h4>
-        </div><!-- /footer -->
-    </div><!-- /page one -->
+    </div>
 
 
     <!-- Start of second page: #two -->
-    <div data-role="page" id="two" data-theme="a">
+    <div data-role="page" id="calendar" data-theme="a">
 
         <div data-role="header">
-            <h1>Two</h1>
+            <h1>Calendar</h1>
 
         </div><!-- /header -->
 
         <div data-role="content" data-theme="a">
-            <h2>Two</h2>
-            <p>I have an id of "two" on my page container. I'm the second page container in this multi-page template.</p>
-            <p>Notice that the theme is different for this page because we've added a few <code>data-theme</code> swatch assigments here to show off how flexible it is. You can add any content or widget to these pages, but we're keeping these simple.</p>
-            <p><a href="#one" data-direction="reverse" data-role="button" data-theme="b">Back to page "one"</a></p>
-
+            <h2>Calendar</h2>
+            <p>Who knew--this would be the calendar.</p>
         </div><!-- /content -->
 
 
@@ -263,25 +272,23 @@ def mobile_body():
     </div><!-- /page two -->
 
 
-    <!-- Start of third page: #popup -->
-    <div data-role="page" id="popup">
+    <div data-role="page" id="quit">
 
         <div data-role="header" data-theme="e">
-            <h1>Dialog</h1>
-
-        </div><!-- /header -->
+            <h1>Quit Synoptic</h1>
+        </div>
 
         <div data-role="content" data-theme="d">
-            <h2>Popup</h2>
-            <p>I have an id of "popup" on my page container and only look like a dialog because the link to me had a <code>data-rel="dialog"</code> attribute which gives me this inset look and a <code>data-transition="pop"</code> attribute to change the transition to pop. Without this, I'd be styled as a normal page.</p>
-            <p><a href="#one" data-rel="back" data-role="button" data-inline="true" data-icon="back">Back to page "one"</a></p>
-        </div><!-- /content -->
+            <h2>Are you sure?</h2>
 
+            Are you sure you want to quit Synoptic Mobile?
 
-        <div data-role="footer">
-            <h4>Page Footer</h4>
-        </div><!-- /footer -->
-    </div><!-- /page popup -->
+            <div data-role="controlgroup">
+            <a href="/app/quit" data-role="button" data-icon="check">Yes</a>
+            <a href="#main" data-role="button" data-icon="delete">No</a>
+            </div>
+        </div>
+    </div>
     """
 
 
