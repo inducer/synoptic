5438d9dbd8d1d3ed25727b2438fe1db66e6b3ef3
Andreas Kloeckner <inform@tiker.net>
2013-03-13 15:24:38 -0400

    Break out README.

--- setup.py
+++ setup.py
@@ -1,47 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: latin1 -*-
+# -*- coding: utf-8 -*-
 
 def main():
     from setuptools import setup
-    import glob
-    import os
-    import os.path
 
     setup(name="synoptic",
           version="2011.1",
           description="An AJAXy notes manager",
-          long_description="""
-          Synoptic is "GMail for your notes". It gives you an efficient and friendly
-          interface that makes it possible to keep and categorize a *large* number of
-          small-ish notes and tidbits of information.
-
-          The following features set it apart:
-
-          * **Fully versioned.** Never deletes *anything*, *ever*. If you want to go back
-            to a previous version of something, just drag that slider up there in the
-            top-left corner.
-
-          * **Super-simple Navigation.** Adaptive tag clouds, 
-            support for the forward/back button on your browser, 
-            query links, support for browser bookmarks. All to make sure
-            you can find that note when you need it.
-
-          * **Powerful searching.** Synoptic is meant to keep *large*
-            note collections manageable and accessible. You can search for items
-            based on tags, their creation time, or even search through their *full text*. 
-            Plus arbitrary logical combinations of them, using the 
-            logical operatos and, or, and not.
-
-          * **Easy Markup.** Synoptic uses
-            Markdown to allow you to type formatted notes easily and
-            quickly. Plus, there are a few extensions to facilitate
-            typing math.
-
-          * **Advanced Features.** A lot of refinement work has gone into
-            making Synoptic work as seamlessly as possible. You may never notice
-            many of these refinements, because they're meant to make stuff work like
-            it's supposed to.
-          """,
+          long_description=open("README.rst", "rt").read(),
           author=u"Andreas Kloeckner",
           author_email="inform@tiker.net",
           license = "MIT",
