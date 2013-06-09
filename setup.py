71833981713e8e0897181abdcdbe933065adf687
Andreas Kloeckner <inform@tiker.net>
2013-06-09 14:18:50 -0400

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ def main():
          )
 
 if __name__ == "__main__":
-    import distribute_setup
-    distribute_setup.use_setuptools()
+    import ez_setup
+    ez_setup.use_setuptools()
 
     main()
